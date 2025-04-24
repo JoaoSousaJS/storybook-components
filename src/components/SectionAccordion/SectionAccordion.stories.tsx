6e99658e44e05929f98c1af6456cc5b61bc16afb
JoaoSousJS <joao.rsousa23@gmail.com>
2025-04-24 20:29:40 +0930

    refactor: change SectionAccordtion storybook

--- src/components/SectionAccordion/SectionAccordion.stories.tsx
+++ src/components/SectionAccordion/SectionAccordion.stories.tsx
@@ -2,6 +2,8 @@ import { Meta, StoryObj } from "@storybook/react";
 import SectionAccordion from "./SectionAccordion";
 import ContactSection from "../ContactSection/ContactSection";
 import { contactMockList } from "../../test/mock/contactMock";
+import { filterContactByStatus } from "../utils/filterContactByStatus";
+
 const meta: Meta<typeof SectionAccordion> = {
   title: "Components/SectionAccordion",
   component: SectionAccordion,
@@ -14,6 +16,19 @@ type Story = StoryObj<typeof SectionAccordion>;
 export const Default: Story = {
   args: {
     title: "Absent",
-    children: <ContactSection contacts={contactMockList} />,
+    children: <ContactSection contacts={filterContactByStatus(contactMockList, "Absent")} />,
+    isOpen: false,
+    onOpen: () => {},
+    onClose: () => {},
+  },
+};
+
+export const Open: Story = {
+  args: {
+    title: "Attented",
+    children: <ContactSection contacts={filterContactByStatus(contactMockList, "Attented")  } />,
+    isOpen: true,
+    onOpen: () => {},
+    onClose: () => {},
   },
 };

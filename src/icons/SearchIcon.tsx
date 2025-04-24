6d57eeca89b2ed287ff375d2ea381988899c2919
JoaoSousJS <joao.rsousa23@gmail.com>
2025-04-24 17:16:24 +0930

    feat: add SearchIcon component

--- src/icons/SearchIcon.tsx
+++ src/icons/SearchIcon.tsx
@@ -12,6 +12,7 @@ export default function SearchIcon({ width = 20, height = 20, color = "#8E9AA5" 
       viewBox="0 0 20 20" 
       fill="none" 
       xmlns="http://www.w3.org/2000/svg"
+      role="img"
     >
       <path
         fillRule="evenodd"

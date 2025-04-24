b7617ac7c398b83aa982b0aabb38b91bf9eb1c4b
JoaoSousJS <joao.rsousa23@gmail.com>
2025-04-24 18:07:33 +0930

    feat: add thumbnail asset

--- src/components/ContactListItem/ContactListItem.test.tsx
+++ src/components/ContactListItem/ContactListItem.test.tsx
@@ -1,19 +1,20 @@
 import { describe, it, expect } from "vitest";
 import { render, screen } from "@testing-library/react";
 import ContactListItem from "./ContactListItem";
+import Thumbnail from '../../assets/thumbnail.png';
 
 describe('ContactListItem', () => {
   it('should render', () => {
-    render(<ContactListItem name="Mary Jane" email="mary.jane@example.com" image="https://via.placeholder.com/150" />);
+    render(<ContactListItem name="Mary Jane" email="mary.jane@example.com" image={Thumbnail} />);
     expect(screen.getByText('Mary Jane')).toBeInTheDocument();
     expect(screen.getByText('mary.jane@example.com')).toBeInTheDocument();
-    expect(screen.getByRole('img')).toHaveAttribute('src', 'https://via.placeholder.com/150');
+    expect(screen.getByRole('img')).toHaveAttribute('src', Thumbnail);
   });
 
   it('should render without email', () => {
-    render(<ContactListItem name="Mary Jane" image="https://via.placeholder.com/150" />);
+    render(<ContactListItem name="Mary Jane" image={Thumbnail} />);
     expect(screen.getByText('Mary Jane')).toBeInTheDocument();
     expect(screen.queryByText('mary.jane@example.com')).not.toBeInTheDocument();
-    expect(screen.getByRole('img')).toHaveAttribute('src', 'https://via.placeholder.com/150');
+    expect(screen.getByRole('img')).toHaveAttribute('src', Thumbnail);
   });  
 });

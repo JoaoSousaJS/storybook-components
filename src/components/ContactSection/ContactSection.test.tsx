3a512538298e43a0e4cc72dbc9e2e3023e6e5199
JoaoSousJS <joao.rsousa23@gmail.com>
2025-04-24 18:57:16 +0930

    test: add testing for ContactSection

---
+++ src/components/ContactSection/ContactSection.test.tsx
@@ -0,0 +1,28 @@
+import { describe, it, expect} from 'vitest';
+import { render, screen } from '@testing-library/react';
+import ContactSection from "./ContactSection";
+import Thumbnail from '../../assets/thumbnail.png';
+describe('ContactSection', () => {
+  it('should render', () => {
+    render(<ContactSection contacts={[
+      { id: '1', name: 'Peter Parker', email: 'peter.parker@example.com', image: Thumbnail, status: 'Attented' },
+    ]} />);
+    expect(screen.getAllByRole('listitem')).toHaveLength(1);
+    expect(screen.getByText('Peter Parker')).toBeInTheDocument();
+  });
+
+  it('should render two contacts', () => {
+    render(<ContactSection contacts={[
+      { id: '1', name: 'Peter Parker', email: 'peter.parker@example.com', image: Thumbnail, status: 'Attented' },
+      { id: '2', name: 'Mary Jane', email: 'mary.jane@example.com', image: Thumbnail, status: 'Absent' },
+    ]} />);
+    expect(screen.getAllByRole('listitem')).toHaveLength(2);
+    expect(screen.getByText('Peter Parker')).toBeInTheDocument();
+    expect(screen.getByText('Mary Jane')).toBeInTheDocument();
+  });
+
+  it('should not render contacts if there are no contacts', () => {
+    render(<ContactSection contacts={[]} />);
+    expect(screen.queryAllByRole('listitem')).toHaveLength(0);
+  });
+});
